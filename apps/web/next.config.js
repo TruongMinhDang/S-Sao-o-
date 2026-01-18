f5bda378223de570344bc669ac1066d267725497
Minh <truongminhdang1@gmail.com>
2026-01-18 07:42:00 +0000

    người ta hoi app này ntn mày nói nghe thư đê họ biết mà sưa lỗi do mày g

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,17 +2,11 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // CHÌA KHÓA VÀNG: Đưa file ra gốc workspace
-  distDir: '../../.next', 
-  
   output: 'standalone',
-
   experimental: {
-    // Gom đủ file thư viện từ gốc
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
-
-  transpilePackages: ['recharts'], 
+  transpilePackages: ['recharts'],
   images: {
     remotePatterns: [
       {
